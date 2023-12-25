bf1be7eaaf53b25c7a441ea38e04af08c36abc9a
Sanilal P Sathyan <sanilal@gmail.com>
2023-12-25 11:00:18 +0400

    Al marai

---
+++ includes/conn.php
@@ -0,0 +1,4 @@
+<?php
+ $url = mysqli_connect("localhost","root","","dbalmarai")  or die("Connection failed");
+ define('TB_pre','md_');
+ ?>
---
+++ includes/side_bar.php
@@ -0,0 +1,65 @@
+   <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          
+           <div class="user-panel">
+            <div class="pull-left image">
+              <img src="dist/img/profile-dummy.jpg" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+              <p><a href="profile.php" >Administartor</p>
+              <!-- Status -->
+              <a href="#"><i class="fa fa-circle text-green"></i> Online</a>
+              
+            </div>
+            
+          </div>
+          
+          <?php 
+          $sid = $_SESSION['user_id']; 
+//           $csql="select `zone_access` from `".TB_pre."admin`WHERE `id`='$sid' ";
+// $cr1=mysqli_query($url,$csql) or die("Failed".mysqli_error($url));
+//$crow=mysqli_fetch_object($cr1);
+//$zone =  $crow->zone_access;
+          ?>
+          <ul class="sidebar-menu">
+          	
+            <li class="header"> <a href="logout.php" ><i class="fa fa-power-off"></i> &nbsp;Sign out</a></li>
+              <li class="treeview <?php if($active=="submissions"){ echo "active";} ?>">
+              <a href="submissions.php">
+                <i class="fa fa-shopping-bag"></i>
+                <span>Shop & Win</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+              	<li><a href="submissions.php"><i class="fa fa-folder-open"></i> View Submissions</a></li>      
+              </ul>
+            </li>
+        <?php /*?>     <li class="treeview <?php if($active=="sections"){ echo "active";} ?>">
+              <a href="#">
+                <i class="fa fa-newspaper-o"></i>
+                <span>Sections</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                  <li>
+                        <a href="edit-section.php?section_id=1">
+                            <i class="fa fa-braille"></i>
+                            <span>Home Section</span>
+                        </a>
+                  </li>
+                  <li>
+                      <a href="edit-section.php?section_id=2">
+                        <i class="fa fa-braille"></i>
+                        <span>About Section</span>
+                      </a>
+                   </li>
+                 </ul>
+           
+             </li><?php */?>
+     
+       		
+          </ul>
+        </section>
+        <!-- /.sidebar -->
+   </aside>
